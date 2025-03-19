331b7a7f47ea89bbc8d2261dda8090b14eddefd4
AndreMaestro <rybalovandrey2007@gmail.com>
2025-03-19 21:02:36 +0400

    fibonacci (0,...,n)

--- first_task.py
+++ first_task.py
@@ -1,7 +1,9 @@
-def fib(n):
-	if n == 1: return 0
-	if n == 2: return 1
-	return fib(n-1)+fib(n-2)
-
 n = int(input())
-print(fib(n))
+f1 = 0
+f2 = 1
+print(f1)
+print(f2)
+for i in range (3, n+1):
+	f3 = f2 + f1
+	f1, f2 = f2, f3
+	print(f3)
